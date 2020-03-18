b9b093e408c40fa6ed01e5256b5bd11dcb9a700f
CarlosGo <56368482+CarlosGo1994@users.noreply.github.com>
2020-03-18 11:19:00 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ Do a program to show the numbers between two numbers user input
 var number = parseInt(prompt("Introduce a first number ",0));
 var number2 =parseInt(prompt("Introduce a second number",0));
 
-document.write("<h2>Since "+ number +" to "+ number2 +" the numbers are :</h2>")
+document.write("<h2>The numbers from"+ number +" to "+ number2 +":</h2>")
 
 for(var i=number; i <= number2; i++)
 {   
